60bf73d4141e580ab1ff916983e3c6424cdbab47
Neranjana Karunaratne <neranjana@Neranjanas-MacBook-Pro.local>
2020-05-16 19:25:59 +1000

    Retrieve video info using ffmpeg

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,9 +1,24 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
 
 app.on('ready', () => {
     console.log('App is now ready');
-    const mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
+
+ipcMain.on('video:submit', (event, path) => {
+    
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        console.log('Video duration is: ', metadata);
+        mainWindow.webContents.send('video:metadata', metadata.format.duration);
+    })
+})
