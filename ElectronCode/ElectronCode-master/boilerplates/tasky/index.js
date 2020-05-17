bd42edb799eb8847bb50459c24f78f9e019a6c05
Neranjana Karunaratne <neranjana@Neranjanas-MacBook-Pro.local>
2020-05-17 18:38:36 +1000

    application on the task bar - initial version

---
+++ ElectronCode/ElectronCode-master/boilerplates/tasky/index.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const electron = require('electron');
+
+const { app, BrowserWindow, Tray } = electron;
+
+let mainWindow;
+let tray;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 500,
+        width:300,
+        frame: false,
+        resizable: false,
+        show: false
+    });
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+    tray = new Tray(iconPath);
+    tray.on('click', (event, bounds) => {
+        // Click event bounds
+        const { x, y } = bounds;
+
+        // Window height and width
+        const {height, width } = mainWindow.getBounds();
+
+        if (mainWindow.isVisible()) {
+            mainWindow.hide();
+        } else {
+            const yPosition = process.platform === 'darwin' ? y : y - height;
+            mainWindow.setBounds({
+                x: x - width/2,
+                y : yPosition, 
+                height, //ES6 for height: height
+                width
+            })
+            mainWindow.show();
+        }
+        
+    });
+});
