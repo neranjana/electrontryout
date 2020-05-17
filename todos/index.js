22cea50b374bf0d427a9d1a7984364b0ef3225a2
Neranjana Karunaratne <neranjana@Neranjanas-MacBook-Pro.local>
2020-05-17 15:53:56 +1000

    todos app with opening windows, garbage collection, menus

---
+++ todos/index.js
@@ -0,0 +1,83 @@
+const electron = require('electron');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+let mainWindow;
+let addWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+    });
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.on('closed', () => app.quit());
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 300,
+        height: 200,
+        title: 'Add New Todo'
+    });
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+    addWindow.on('colosed', () => addWindow = null);
+}
+
+ipcMain.on('todo:add', (event, todo) => {
+    console.log('to do is ', todo);
+    mainWindow.webContents.send('todo:add', todo);
+    addWindow.close();
+});
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            { 
+                label: 'New Todo' ,
+                click() { createAddWindow(); }
+            },
+            { 
+                label: 'Clear Todos' ,
+                click() { mainWindow.webContents.send('todo:clear'); }
+            },
+            {
+                label: 'Quit', 
+                accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+if (process.platform === 'darwin') {
+    menuTemplate.unshift({label: ''}); // for mac os 
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    menuTemplate.push({
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    })
+}
+
+//production, development, staging, test
