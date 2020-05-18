af29d018a284a995825270bd791f9dc099f578f6
Neranjana Karunaratne <neranjana@Neranjanas-MacBook-Pro.local>
2020-05-18 14:50:50 +1000

    Refactor MainWindow in to its own class

--- ElectronCode/ElectronCode-master/boilerplates/tasky/index.js
+++ ElectronCode/ElectronCode-master/boilerplates/tasky/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const electron = require('electron');
 const TimerTray = require('./app/timer_tray');
+const MainWindow = require('./app/main_window');
 
 const { app, BrowserWindow, Tray } = electron;
 
@@ -9,17 +10,7 @@ let tray; // to prevent the tray being garbage collected.
 
 app.on('ready', () => {
     app.dock.hide(); // hide the icon in the dock since this is a tray based application.
-    mainWindow = new BrowserWindow({
-        height: 500,
-        width:300,
-        frame: false,
-        resizable: false,
-        show: false
-    });
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-    mainWindow.on('blur', () => {
-        mainWindow.hide(); // hide the window when the user clicks somewhere else
-    })
+    mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
 
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
